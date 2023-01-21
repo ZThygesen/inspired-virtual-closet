b340bf5033d17664436ebf5ca40c931661d7e7c5
ZThygesen <zsthygesen@gmail.com>
2023-01-21 14:50:17 -0600

    add basic home screen

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,15 +4,50 @@ import logo from '../images/big_logo.png';
 
 const Container = styled.div`
     flex-grow: 1;
-    display: flex;
+    align-self: center;
+    width: 100%;
+    max-width: 1000px;
+    display: grid;
+    grid-template-columns: 50% 50%;
 
     .big-logo-img {
-        background-image: url(${logo});
-        background-repeat: no-repeat;
-        background-size: contain;
-        background-position: center;
-        width: 100%;
-        height: 800px;
+        align-self: center;
+        max-width: 100%;
+        max-height: calc(90vh - var(--header-height));
+    }
+
+    .home-options {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        gap: 50px;
+    }
+
+    p {
+        font-size: 100px;
+        font-family: 'Mallows';
+        color: var(--black);
+    }
+
+    a {
+        background-color: var(--secondary);
+        text-decoration: none;
+        color: var(--black);
+        font-family: 'Fashion';
+        font-size: 38px;
+        letter-spacing: 1px;
+        padding: 20px;
+        border: 2px solid var(--black);
+        border-radius: 25px;
+        font-weight: bold;
+        transition: all 0.1s;
+        margin: 10px;
+
+        &:hover {
+            background-color: var(--secondary-light);
+        }
     }
 
 `;
@@ -20,8 +55,13 @@ const Container = styled.div`
 export default function Home() {
     return (
         <Container>
-            <div className="big-logo-img"></div>
-            <Link to="manage-clients">Manage Clients</Link>
+                <img src={logo} alt="logo" className="big-logo-img" />
+                <div className="home-options">
+                    <div className="site-description">
+                        <p>Digital Closet</p>
+                    </div>
+                    <Link to="manage-clients">MANAGE CLIENTS</Link>
+                </div>
         </Container>
     );
 }
