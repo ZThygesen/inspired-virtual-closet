15d2600d15e5bd41192a95f283e1e8c41d3ead1f
ZThygesen <zsthygesen@gmail.com>
2024-01-10 17:42:15 -0600

    add requirements for deployment

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ import express from 'express';
 import { config } from 'dotenv';
 import { mongoConnect } from './mongoConnect.js';
 import { bucketConnect } from './bucketConnect.js';
+import path from 'path';
 
 const app = express();
-const port = 5000;
 app.use(express.json());
 config();
+const port = process.env.PORT || 5000;
 
 // connect to mongo db
 let db;
@@ -54,11 +55,18 @@ app.post('/password', async (req, res, next) => {
     }
 });
 
-app.use((err, req, res, next) => {
-    console.log(`error: ${err.message}, status: ${err.status}`);
-    const status = err.status || 500;
-    res.status(status).json({ message: err.message });
-});
+if (process.env.NODE_ENV === 'review' || process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, './client/build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+    });
+} else {
+    app.use((err, req, res, next) => {
+        console.log(`error: ${err.message}, status: ${err.status}`);
+        const status = err.status || 500;
+        res.status(status).json({ message: err.message });
+    });
+}
 
 app.listen(process.env.port || port, () => console.log(`Server started on port ${process.env.port || port}`));
 
