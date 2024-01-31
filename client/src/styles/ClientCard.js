a024f611255b236caea97b7fb6af5145420b12b0
ZThygesen <zsthygesen@gmail.com>
2024-01-31 15:09:04 -0600

    attempt mobile scroll fix

--- client/src/styles/ClientCard.js
+++ client/src/styles/ClientCard.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const ClientCardContainer = styled.div`
+    height: fit-content;
     display: flex;
     flex-direction: column;
     align-items: center;
