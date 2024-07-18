49963135c9ec84df5089d1c8a81e2c0b5fbdb397
ZThygesen <zsthygesen@gmail.com>
2024-07-18 15:35:37 -0500

    remove comma in copyright notice

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -82,7 +82,7 @@ export default function Home() {
                     }
                 </div>
             </HomeContainer>
-            <p className="copyright">© 2024, Edie Styles, LLC</p>
+            <p className="copyright">© 2024 Edie Styles, LLC</p>
             <Loading open={loading} />
         </>
     );
